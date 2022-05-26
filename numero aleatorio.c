4f199e79b121efd8b5022e8334f939fab345d163
Dogola <lucasdogo01@gmail.com>
2022-05-26 14:16:05 -0300

    Numero aleatorio

---
+++ numero aleatorio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(){
+
+    // Gerador de numero aleatorio 
+
+    srand ( (unsigned) time (NULL));
+
+    //variavel que recebe o numero aleatorio
+     int a;
+     a = rand() % 11;
+
+     printf (" o numero e: \n %d \n", a);
+
+}

ebf8a570228030e50260831ba9c0e418ca3194b6
Dogola <lucasdogo01@gmail.com>
2022-05-26 15:03:34 -0300

    for

---
+++ for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    // Contando com FOR
+    int a;
+    for (a = 1; a <= 10; a++)
+    {
+        printf("\n contando ate 10: %d \n", a);
+    }
+}

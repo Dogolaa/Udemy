c1eb9752a02fa471ba9d28aaa28a6aaee46e4c8d
Dogola <lucasdogo01@gmail.com>
2022-05-26 14:01:19 -0300

    Primeiro programa em C

---
+++ calculadora.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    float valor1, valor2;
+    int opcao;
+
+    printf("Escreva o primeiro valor \n");
+    scanf("%f", &valor1);
+    printf("Escreva o segundo valor \n");
+    scanf("%f", &valor2);
+    printf("------CALCULADORA----- \n");
+    printf("\n Opcao 1 - Somar \n Opcao 2 - Subtrair \n Opcao 3 - Multiplicar \n Opcao 4 - Dividir \n");
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case 1:
+        printf("Resultado da soma: %.2f \n", valor1 + valor2);
+        break;
+    case 2:
+        printf("Resultado da subtracao: %.2f \n", valor1 - valor2);
+        break;
+    case 3:
+        printf("O resultado da multiplicacao: %.2f \n", valor1 * valor2);
+        break;
+    case 4:
+        printf("O valor da divisao: %.2f \n", valor1 / valor2);
+
+    default:
+        printf("Opcao invalida \n");
+
+        break;
+    }
+}
+ git config --global user.email "lucasdogo01@gmail.com"
+  git config --global user.name "Dogola"

121b04a4f9e33ac05c7a6ff908cc59987bf13e85
Dogola <lucasdogo01@gmail.com>
2022-05-31 15:39:35 -0300

    Primo

--- Codigos/primo.c
+++ Codigos/primo.c
@@ -4,25 +4,40 @@
 void main()
 {
     int a, b, cont;
-
-    printf("Insira um valor para descobrir se o numero e primo ou nao \n");
-    scanf("%d", &a);
-    cont = 0;
-
-    for (b = 1; b <= a; b++)
+    char opcao;
+    printf("Deseja verificar se um numero e primo ? (s/n)");
+    scanf("%c", &opcao);
+    while (opcao == 's')
     {
-        printf("O resto entre %d e %d eh = %d \n", a, b, a % b);
-        if (a % b == 0)
+
+        printf("Insira um valor para descobrir se o numero e primo ou nao \n");
+        scanf("%d", &a);
+        cont = 0;
+
+        switch (opcao)
         {
-            cont++;
+        case 1:
+            
+            break;
+
+        for (b = 1; b <= a; b++)
+        {
+            printf("O resto entre %d e %d eh = %d \n", a, b, a % b);
+            if (a % b == 0)
+            {
+                cont++;
+            }
         }
+        if (cont == 2)
+        {
+            printf("-----O NUMERO %d EH PRIMO-----\n", a);
+        }
+        else
+        {
+            printf("-----O NUMERO %d  NAO EH PRIMO-----\n", a);
+        }
+
+
     }
-    if (cont == 2)
-    {
-        printf("-----O NUMERO %d EH PRIMO-----\n", a);
-    }
-    else
-    {
-        printf("-----O NUMERO %d  NAO EH PRIMO-----\n", a);
-    }
+
 }

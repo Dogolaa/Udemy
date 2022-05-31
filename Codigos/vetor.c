96a8cea8b3cc2b0da13efaeb3f8c864f05502eac
Dogola <lucasdogo01@gmail.com>
2022-05-31 14:23:11 -0300

    vetor

---
+++ Codigos/vetor.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(){
+     int vetor[10], cont;
+    
+     for(cont = 0; cont < 10; cont++){
+         printf ("Digite o valor do vetor %d", cont);
+         scanf ("%d", vetor[cont]);
+     }
+  
+}
+
+git config --global user.email lucasdogo01@gmail.com
+  git config --global user.name Dogola

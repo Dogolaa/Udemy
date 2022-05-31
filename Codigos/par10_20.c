d6550083f077f2073c23efdc2e7b42ab8a87c121
Dogola <lucasdogo01@gmail.com>
2022-05-31 14:53:58 -0300

    Numeros  Pares entre 10 e 20

---
+++ Codigos/par10_20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int a;
+    for (a = 10; a <= 20; a = a + 2)
+    {
+        printf("Os valores pares entre 10 e 20 sao: %d \n", a);
+    }
+}
